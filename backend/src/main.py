a51b2b171eafeb98adf938477cd0913e3d711182
Fedor Vorobyov <fedorvorobev@gmail.com>
2025-03-19 18:02:15 +0300

    mkdir static

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 from .database import create_db_and_tables
 from .routers import auth, words, progress
 from .config import settings
 
 app = FastAPI(title="Zubroslov API")
+
+# create a static directory to store the static files
+static_dir = Path('./static')
+static_dir.mkdir(parents=True, exist_ok=True)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Настройка CORS
